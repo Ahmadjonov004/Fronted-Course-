e12bc1ccb965092bc80418cfb518d12a02bd815e
Ahmadjonov004 <ahmadjonovasilbek624@gmail.com>
2025-02-18 10:16:35 +0500

    bugungi homeworkni boshladim

--- Desktop/fronted kurs/React/Component-Composition/src/App.tsx
+++ Desktop/fronted kurs/React/Component-Composition/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './index.css'
 import './App.css'
 import DeleteModal from "./components/deleteModal/deleteModal"
 import EditModal from "./components/editModal/editModal"
@@ -20,8 +21,8 @@ function App() {
 
   return (
     <div className="app">
-      <button onClick={openDeleteModal}>Open Delete Modal</button>
-      <button onClick={openEditMOdal}>Open Edit Modal</button>
+      <button onClick={openDeleteModal} className="openDeleteModal">Open Delete Modal</button>
+      <button onClick={openEditMOdal} className="openEditModal">Open Edit Modal</button>
       {editModal && <EditModal/>}
       {deleteModal && <DeleteModal/>}
     </div>
